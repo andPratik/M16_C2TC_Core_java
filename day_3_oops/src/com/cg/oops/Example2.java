7b085bdee7298d52a87c4878ef6a2c87228e24b4
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-04-11 16:32:24 +0530

    frist commit

---
+++ day_3_oops/src/com/cg/oops/Example2.java
@@ -0,0 +1,10 @@
+package com.cg.oops;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		/
+
+	}
+
+}
---
+++ day_3_oops/src/com/cg/oops/car.java
@@ -0,0 +1,15 @@
+package com.cg.oops;
+
+public class car {
+	String name = " bmw" ;
+	 void print() {
+		 System.out.println("this is name"+name);
+	 }
+
+	public static void main(String[] args) {
+		car obj = new car();
+		obj.print();
+
+	}
+
+}
