a434e9708373e91ce75d51ed4260d313f7f1c194
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-04-12 10:59:49 +0530

    first commit

--- day_3_oops/src/com/cg/oops/Example2.java
+++ day_3_oops/src/com/cg/oops/Example2.java
@@ -1,9 +1,13 @@
 package com.cg.oops;
+// constructor with parameter
 
 public class Example2 {
+	Example2(String name){
+		System.out.println("CAR NAME :"+ name);
+	}
 
 	public static void main(String[] args) {
-		/
+		Example2 obj = new Example2("bmw");
 
 	}
 
--- day_3_oops/src/com/cg/oops/Example3.java
+++ day_3_oops/src/com/cg/oops/Example3.java
@@ -1,9 +1,21 @@
 package com.cg.oops;
 
 public class Example3 {
+	String name ;
+	Example3(String name){
+		this.name=name ;
+	}
+	void disply() {
+		System.out.println("this is name "+ name );
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Example3 E1 =new Example3("audi");
+		Example3 E2 =new Example3("bmw");
+		Example3 E3 =new Example3("tesla");
+		E1.disply();
+		E2.disply();
+		E3.disply();
 
 	}
 
