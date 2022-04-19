2cc77ae7e915bbf1a1d4d12a3799b150d5b7bc47
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-04-19 14:31:56 +0530

    frist commit

---
+++ day_7_this_static_instanceof/src/com/cg/static1/Example2.java
@@ -0,0 +1,15 @@
+package com.cg.static1;
+
+public class Example2 
+{
+	static void display()
+	{
+		System.out.println("hello world ");
+	}
+
+	public static void main(String[] args) {
+		
+		display();
+
+	}
+}
---
+++ day_7_this_static_instanceof/src/com/cg/static1/Example3.java
@@ -0,0 +1,23 @@
+package com.cg.static1;
+
+public class Example3 
+{
+	static int x;
+	static String str;
+	static
+	{
+		x=10;
+		str="pratik";
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(x);
+		System.out.println(str);
+
+
+	}
+}
