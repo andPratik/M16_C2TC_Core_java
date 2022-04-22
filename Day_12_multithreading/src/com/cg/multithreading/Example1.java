3d5e0cf808aa74fdd2f4ecc5187fc82a702dd46f
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-04-22 21:24:31 +0530

    first commit

---
+++ Day_12_multithreading/src/com/cg/multithreading/Example1.java
@@ -0,0 +1,18 @@
+package com.cg.multithreading;
+
+public class Example1 {
+	public static void main(String args[])
+	{
+		Thread th=Thread.currentThread();
+		System.out.println("Current Thread:"+th);
+		try
+		{
+			Thread.sleep(1);
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
