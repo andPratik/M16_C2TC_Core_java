d94f14f103e650df4ee42687c454646a866a760d
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-04-27 23:19:18 +0530

    first commit

--- Day_2_introd_to_java/src/com/cg/model/Example2.java
+++ Day_2_introd_to_java/src/com/cg/model/Example2.java
@@ -6,6 +6,7 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
 		int num = sc.nextInt();
 		System.out.println(num);
 
