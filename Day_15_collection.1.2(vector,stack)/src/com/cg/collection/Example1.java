a7402f9224fb64d71e25534e3eba52c6cdd6b2c7
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-05-03 09:10:58 +0530

    First commit

---
+++ Day_15_collection.1.2(vector,stack)/src/com/cg/collection/Example1.java
@@ -0,0 +1,14 @@
+package com.cg.collection;
+
+import java.util.Vector;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		v.add(23);
+		 v.add("someting");
+		 System.out.println(v);
+	}
+
+}
---
+++ Day_15_collection.1.2(vector,stack)/src/com/cg/collection/Example2.java
@@ -0,0 +1,19 @@
+package com.cg.collection;
+
+import java.util.Stack;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Stack s = new Stack();
+		s.push(23);
+		s.push(56);
+		s.add(28);
+		s.add(54);
+		s.pop();
+		s.push(29);
+		System.out.println(s);
+
+	}
+
+}
