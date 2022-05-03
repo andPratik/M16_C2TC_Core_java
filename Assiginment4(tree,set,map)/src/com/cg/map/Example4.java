a89fd3b069931437c79518fc82ca3fc86e052b3c
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-05-03 09:10:03 +0530

    First commit

---
+++ Assiginment4(tree,set,map)/src/com/cg/map/Example4.java
@@ -0,0 +1,15 @@
+package com.cg.map;
+
+import java.util.SortedSet;
+
+public class Example4 {
+	public static void main(String[] args) {
+		SortedSet sortedSet = new SortedSet();
+		SortedSet<Integer> ss = sortedSet ;
+		ss.add(11);
+		ss.add(76);
+		ss.add(67);
+		System.out.println(ss);
+	}
+
+}
