a3f22605479533d65d63d1cecb206bbecf0b4bf1
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-05-17 11:08:17 +0530

    first commit

--- Day_2_introd_to_java/src/com/cg/model/Example2.java
+++ Day_2_introd_to_java/src/com/cg/model/Example2.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Example2 {
 
 	public static void main(String[] args) {
+		//@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the number");
 		int num = sc.nextInt();
 		System.out.println(num);
+		sc.close();
 
 	}
 
