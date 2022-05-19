86ccd1e4d373677b9640c1c401764a35f873fc4e
LENOVO <LENOVO@DESKTOP-7AMLKCL>
2022-05-19 14:42:02 +0530

    first commit

---
+++ Day _24_Lamnda_Expression/src/com/cg/lambda/Example2.java
@@ -0,0 +1,23 @@
+
+package com.cg.lambda;
+
+interface B
+{
+	String display(String str);
+}
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		//Lambda Expression with return statement
+		//Lambda Expression with Single Parameter
+		B obj=(String str)->
+		{
+			
+			return str;
+		};
+		System.out.println(obj.display("Lambda Expression with Single Parameter"));
+
+	}
+
+}
